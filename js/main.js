b97cd32bf9e05dc54b917649aa789d0a82362f5b
Vivek Raja P S <vivekraja98@gmail.com>
2021-04-13 20:20:45 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@
     /*==================================================================
     [ Validate ]*/
 
-    var email = $('.validate-input input[name="email"]');
 
-    $('.validate-form').on('submit',function(){
+
+    $('.contact1-form-btn').on('click',function(){
+        var email = $('#susemail').val();
         var check = true;
 
         if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
@@ -19,6 +20,17 @@
         return check;
     });
 
+    $('.contact2-form-btn').on('submit',function(){
+         var email = $('#unsusemail').val();
+        var check = true;
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        return check;
+    });
 
     $('.validate-form .input1').each(function(){
         $(this).focus(function(){
